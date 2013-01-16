63ca8ecb22ed1e83650b04a6aad668cfb10f375a
Anirban Das <anirban.nick@gmail.com>
2013-01-16 20:27:52 +0530

    Adding Scrappy to repo.

---
+++ Corpus Generator/Scrappy/TOI_Crawler/pipelines.py
@@ -0,0 +1,21 @@
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: http://doc.scrapy.org/topics/item-pipeline.html
+
+from scrapy.exceptions import DropItem
+import json
+
+class ToiCrawlerPipeline(object):
+    i,j=0,0
+    def __init__(self):
+	      self.i=self.i+1
+	      self.file = open('toi_sports_cricket_%s.jl'%self.i, 'wb')
+    
+    def process_item(self, item, spider):
+	      self.j=self.j+1
+	      
+	      self.file = open('toi_sports_cricket_%s.jl'%self.j, 'wb')
+	      line = json.dumps(dict(item)) + "\n"
+	      self.file.write(line)
+	      return item
---
+++ Corpus Generator/Scrappy/TOI_Crawler/settings.py
@@ -0,0 +1,22 @@
+# Scrapy settings for TOI_Crawler project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/topics/settings.html
+#
+
+BOT_NAME = 'TOI_Crawler'
+
+SPIDER_MODULES = ['TOI_Crawler.spiders']
+NEWSPIDER_MODULE = 'TOI_Crawler.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'TOI_Crawler (+http://www.yourdomain.com)'
+
+DEFAULT_ITEM_CLASS = 'TOI_Crawler.items.ToiCrawlerItem'
+
+ITEM_PIPELINES = ['TOI_Crawler.pipelines.ToiCrawlerPipeline']
+DUPEFILTER_CLASS = 'scrapy.dupefilter.RFPDupeFilter'
+
+
