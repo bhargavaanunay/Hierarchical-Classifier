3d8571102001ab67e514e7793ef738cf47dd3b39
Anirban Das <anirban.nick@gmail.com>
2013-01-17 00:32:01 +0530

    Removed Unnecessary statement from pipeline.py

--- Corpus Generator/Scrappy/TOI_Crawler/pipelines.py
+++ Corpus Generator/Scrappy/TOI_Crawler/pipelines.py
@@ -7,10 +7,10 @@ from scrapy.exceptions import DropItem
 import json
 
 class ToiCrawlerPipeline(object):
-    i,j=0,0
+    i,j=10,0
     def __init__(self):
 	      self.i=self.i+1
-	      self.file = open('toi_sports_cricket_%s.jl'%self.i, 'wb')
+	      #self.file = open('toi_sports_cricket_%s.jl'%self.i, 'wb')
     
     def process_item(self, item, spider):
 	      self.j=self.j+1
