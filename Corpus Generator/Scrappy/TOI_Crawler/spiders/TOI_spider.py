5d7fbf6e5a545ebfc948b2210b3ad92b745b5ede
Ayush Jaiswal <mail.ayush.jaiswal@gmail.com>
2013-01-17 14:12:31 +0530

    Improved readability.

--- Corpus Generator/Scrappy/TOI_Crawler/spiders/TOI_spider.py
+++ Corpus Generator/Scrappy/TOI_Crawler/spiders/TOI_spider.py
@@ -1,7 +1,6 @@
 from scrapy.spider import BaseSpider
 from scrapy.selector import HtmlXPathSelector
 from scrapy.http import Request
-
 from TOI_Crawler.items import ToiCrawlerItem
     
 
@@ -9,58 +8,45 @@ class ToiCrawlerSpider(BaseSpider):
 
     name = "Toi"
     allowed_domains = ["timesofindia.indiatimes.com"]
-    print "\n\nEnter URL of the topic page:",
+    print "\nEnter URL of the topic page:",
     seed = raw_input()
     start_urls = [seed]
-        #"http://timesofindia.indiatimes.com/sports/cricket/sphome/4719161.cms",]
     idx1 = seed.find('.com/') + 5
     idx2 = seed.find('/', idx1 + 1)
     idx2 = seed.find('/', idx2 + 1)
     sp_category = seed[idx1:idx2]
 
     def parse(self, response):
-        #print "\n\n\n\n\nRESPONSE ",response.url,"\n\n\n\n\n"
-        w=ToiCrawlerItem()
+        w = ToiCrawlerItem()
         currentURL = response.url
-        #print "\n\n\n",currentURL,"\n\n\n"
-        if  currentURL.find('/articleshow/')!=-1:
+        if currentURL.find('/articleshow/') != -1:
             hxs = HtmlXPathSelector(response)
             title = hxs.select('//title/text()').extract()
-            #print "\n\n\n",title,"\n\n\n"
             w['title']= title
             content = hxs.select('//tmp/text()').extract()
-            #print "\n\n\n",content, "\n\n\n"
-            content1=str(content)
-            if content1.find('a')==-1:
+            content1 = str(content)
+            if content1.find('a') == -1:
                  con = hxs.select('//div[contains(@class,"Normal")]')
-                 if len(con)>1:
+                 if len(con) > 1:
                      print "\n\n\nHELLO HELLO HELLO\n\n\n"
                  else:
-                     content=con.select('text()').extract()
+                     content = con.select('text()').extract()
                      w['content'] = content
-                     w['url']=currentURL
-                     yield  w
+                     w['url'] = currentURL
+                     yield w
             else:
-                 w['content']=content
-                 w['url']=currentURL
+                 w['content'] = content
+                 w['url'] = currentURL
                  yield w
         hxs = HtmlXPathSelector(response)
-        sites = hxs.select('//a[contains(@href,"/%s")]/@href' %self.sp_category).extract()
+        sites = hxs.select('//a[contains(@href,"/%s")]/@href' % self.sp_category).extract()
         
         for site in sites:
-            #print "\n\n\n",site,"\n\n\n"
-            #u=site.select('@href').extract()
-            #print "\n\n\n",u[0],"\n\n\n"
-            site=str(site)
-            #print "\n\n\n",site,"\n\n\n"
-            if site.find('video/%s' %self.sp_category)== -1:
+            site = str(site)
+            if site.find('video/%s' % self.sp_category) == -1:
                  if site.find('http://')==-1:
-                    site= "http://timesofindia.indiatimes.com"+ site
+                    site = "http://timesofindia.indiatimes.com" + site
                     print "\n\n\n", site, "\n\n\n"
                     yield Request(site, callback=self.parse)
                  else:
-                    yield Request(site,callback=self.parse) 
-            
-           
-            #s="http://timesofindia.indiatimes.com/sports/cricket/top-stories/2nd-Test-South-Africa-complete-innings-win-over-New-Zealand/articleshow/18019385.cms"
-                   
+                    yield Request(site, callback=self.parse)             
